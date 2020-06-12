983cb02669f64c72cf9cf7e6e2224823747e7c8d
Cristian González <cristian.glez.m@gmail.com>
2020-06-12 23:10:49 +0200

    final version deplyToGithub.sh

--- deployToGithub.sh
+++ deployToGithub.sh
@@ -1,35 +1,28 @@
 #!/bin/bash
-echo "write the backend: (https://example.com)"
-read BACKEND
-echo "write the BASE_URL: ('/')"
-read BASE_URL
-# backup .env
-mv .env .env_bak
 
+BACKEND=$1
+BASE_URL=$2
+# backup .env
+echo "making backup of .env"
+mv .env .env_bak
 # build .env
+echo "Building .env file"
 echo "VUE_APP_IMAGES_URL=$BACKEND/generated/" > .env
 echo "VUE_APP_API_URL=$BACKEND/api/" >> .env
 echo "VUE_APP_DOWNLOAD_URL=$BACKEND/download/" >> .env
 echo "VUE_APP_BASE_URL=$BASE_URL" >> .env
-
-echo "This is the configuration: "
-cat .env
-echo "want to continue?(y/n)"
-read cont
-if [ "$cont" == "y" ]
-	then
-	echo "Building..."
-	npm run build
-	git stash
-	git checkout --orphan gh-pages
-	git stash pop
-	git add dist/
-	git commit
-	git subtree push --prefix dist origin gh-pages
-	git checkout master
-else
-	echo "Aborting..."
-fi
+git checkout --orphan gh-pages
+echo "building..."
+npm run build
+echo "restoring backup"
 # restore backup
 rm .env
 mv .env_bak .env
+git --work-tree dist add --all
+git --work-tre dist commit -m "gh-pages"
+echo "pushing to gh-pages..."
+git push origin HEAD:gh-pages --force
+rm -R dist
+git checkout -f master
+git branch -D gh-pages
+echo "Deployment done"
