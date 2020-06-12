f91133364a5fe4f4c2dd2626ebd5fddcb0beeb2e
Cristian González <cristian.glez.m@gmail.com>
2020-06-12 16:10:32 +0200

    fixed deployToGithub.sh

--- deployToGithub.sh
+++ deployToGithub.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
 echo "checkout to gh-pages"
+git stash
 git checkout gh-pages
 echo "write the backend: (https://example.com)"
-read HOST
+read BACKEND
 echo "write the BASE_URL: ('/')"
 read BASE_URL
 # backup .env
 mv .env .env_bak
+
 # build .env
-echo "VUE_APP_IMAGES_URL=$HOST/generated/" > .env
-echo "VUE_APP_API_URL=$HOST/api/" >> .env
-echo "VUE_APP_DOWNLOAD_URL=$HOST/download/" >> .env
+echo "VUE_APP_IMAGES_URL=$BACKEND/generated/" > .env
+echo "VUE_APP_API_URL=$BACKEND/api/" >> .env
+echo "VUE_APP_DOWNLOAD_URL=$BACKEND/download/" >> .env
 echo "VUE_APP_BASE_URL=$BASE_URL" >> .env
-echo "This is the configuration:"
+
+echo "This is the configuration: "
 cat .env
 echo "want to continue?(y/n)"
 read cont
-if [ cont -eq "y" ]
+if [ $cont -eq "y" ]
 	then
 	echo "Building..."
 	npm run build
@@ -32,3 +35,4 @@ rm .env
 mv .env_bak .env
 echo "checkout master"
 git checkout master
+git stash pop
