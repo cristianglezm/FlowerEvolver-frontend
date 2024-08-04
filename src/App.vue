d0a3b80bf8f3baedac0b0313630a422d7229ad99
Cristian Gonzalez <cristian.glez.m@gmail.com>
2024-08-04 23:26:43 +0200

    added feature image-caption
    * added transformers.js
    * added descriptions to db
    * added AiStore, captioner worker, WorkerManager
    * added describe button to flowers
    * added logic to load descriptions in local and favourites.
    * updated TODO
    * added descriptionModal component
    * added loadModal option
    * added load modal option in Settings
    * moved progress bar to app.vue
    * fixed bug in deleteNonFavourites in which it deleted the 2 flower
    * keep descriptions of favourites when deleting all flowers but favourites.
    * added LoadModel, descriptionModal to App.vue
    * added progress bar when loading vit-gpt2 model
    * replaced errors to show description for flower
    * fixed css
    * refactored AppTitle, Settings to use WorkerManager
    * added a MultiProgressModal to give better information to user.
    * update TODO
    * fixed license comment not showing

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app-container">
+    <ProgressModal :id="'progressBar'" :channel="emitter" :on="'showProgress'" :update="'updateProgress'" />
+    <MultiProgressNodal :id="'multiProgressBar'" :channel="emitter" :on="'requestMultiProgressBar'" />
+    <DescriptionModal :id="'descriptionModal'" :channel="emitter" :on="'showDescriptionModal'" :update="'updateDesc'" />
     <AppTitle />
     <AppMenu :is-local="isLocal()" />
     <router-view :key="routes.fullPath" class="view" />
@@ -9,12 +12,54 @@
 
 <script setup>
 
+import { onMounted, inject } from 'vue';
+import ProgressModal from './components/ProgressModal.vue';
+import MultiProgressNodal from './components/MultiProgressModal.vue';
+import DescriptionModal from './components/DescriptionModal.vue';
 import AppTitle from './components/AppTitle.vue';
 import AppMenu from './components/AppMenu.vue';
 import AppFooter from './components/AppFooter.vue';
 import { useRoute } from 'vue-router';
+import { Captioner } from './store/AIStore/AI';
+import { useFlowersStore } from './store';
 
 const routes = useRoute();
+const emitter = inject('emitter');
+const store = useFlowersStore();
+
+emitter.on('loadModel', () => {
+  setTimeout(() => {
+      emitter.emit('requestMultiProgressBar', {
+            status: "setup",
+            title: "downloading or loading model for describing flowers",
+            onLoad: async () => {
+              Captioner.getInstance((data) => {
+                  switch(data.status){
+                    case "initiate":{
+                        let event = {
+                          status: "init",
+                          name: data.file,
+                          progress: 0,
+                          total: 100
+                        };
+                        emitter.emit('requestMultiProgressBar', event);
+                    }
+                      break;
+                    case "progress":{
+                        let event = {
+                          status: "update",
+                          name: data.file,
+                          progress: data.progress
+                        };
+                        emitter.emit('requestMultiProgressBar', event);
+                    }
+                      break;
+                  }
+              });
+            }
+      });
+    }, 2000);
+});
 
 const isLocal = () => {
   return routes.path === '/Local' || 
@@ -22,16 +67,21 @@ const isLocal = () => {
           routes.params.isLocal === 'local' || 
           routes.path === '/Settings';
 };
+onMounted(() => {
+  if(store.settings.loadModel){
+    emitter.emit('loadModel');
+  }
+})
 
+  /*!
+   * @license SIL Open Font License 1.1 - Copyright (c) 2023, GitHub
+   * https://github.com/githubnext/monaspace
+   * with Reserved Font Name "Monaspace", including subfamilies: "Argon", "Neon", "Xenon", "Radon", and "Krypton"
+   */
 </script>
 
 <style>
 @font-face {
-  /* 
-            SIL Open Font License 1.1 - Copyright (c) 2023, GitHub
-            https://github.com/githubnext/monaspace
-            with Reserved Font Name "Monaspace", including subfamilies: "Argon", "Neon", "Xenon", "Radon", and "Krypton"
-  */
   font-family: 'MonaspaceRadon-Regular';
   src: url('/MonaspaceRadon-Regular.otf') format('opentype');
   font-weight: normal;
