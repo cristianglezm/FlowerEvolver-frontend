b1511ed0fa5bf95ac396cbdc450017c12f7bedd9
Cristian Gonzalez <cristian.glez.m@gmail.com>
2024-10-12 15:05:25 +0200

    fix github pages 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 import Local from '../views/Local.vue';
 import LastAdded from '../views/LastAdded.vue';
@@ -31,7 +31,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
 
