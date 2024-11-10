8914a6d57c103b53bc32969171fcb75340f53bd7
Cristian Gonzalez <cristian.glez.m@gmail.com>
2024-11-10 23:59:21 +0100

    refactored settings
    * added isModelInCache to ModelOptions.vue
    * added HasFiles to CacheManager class
    * added AppActions
    * added AppOptions
    * added CreationParams
    * added MutationRates

---
+++ src/components/AppActions.vue
@@ -0,0 +1,378 @@
+<template>
+  <UploadFileModal :id="'importModal'" :channel="emitter" :on="'showImport'" />
+  <div id="actions-settings">
+    <div id="actions-safe">
+      <h2 style="color:lightgreen;">Actions</h2>
+      <button class="safe-button" @click="showRedrawFlowers()"> Redraw local Flowers </button>
+      <div class="columns">
+        <div class="column">
+          <button class="safe-button" @click="showExport('favourites')"> Export favourite flowers </button>
+          <button class="safe-button" @click="showExport('garden')"> Export garden Flowers </button>
+          <button class="safe-button" @click="showExport('all')"> Export local Flowers </button>
+        </div>
+        <div class="column">
+          <button class="safe-button" @click="showImport(true)"> Import Flowers to favourites </button>
+          <button class="safe-button" @click="importGarden()"> Import garden Flowers </button>
+          <button class="safe-button" @click="showImport(false)"> Import Flowers </button>
+        </div>
+      </div>
+    </div>
+    <div id="actions-danger">
+      <h2 style="color:red;">Danger Zone</h2>
+      <button class="unsafe-button" @click="deleteAllFlowers()"> Delete All Flowers </button>
+      <button class="unsafe-button" @click="deleteNonFavourites()"> Delete non Favourites </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { inject, toRaw, onBeforeUnmount } from 'vue';
+import { useFlowersStore, STORAGE_KEY_GARDEN } from '../store';
+import UploadFileModal from '../components/UploadFileModal.vue';
+import redrawWorker from '../workers/redraw.worker?worker';
+import exportWorker from '../workers/export.worker?worker';
+import importWorker from '../workers/import.worker?worker';
+import WorkerManager from '../store/WorkerManager';
+import mitt from 'mitt';
+
+const store = useFlowersStore();
+let emitter = inject('emitter');
+let channel = mitt();
+let wm = new WorkerManager(channel);
+
+wm.addWorker('redraw', redrawWorker());
+wm.addWorker('exporter', exportWorker());
+wm.addWorker('importer', importWorker());
+
+const onError = (e) => {
+    store.errors.push({ message: e});
+};
+wm.onError('redraw', onError);
+wm.onResponse('redraw', (e) => {
+    emitter.emit('updateProgress', {
+        progress: e.progress,
+    });
+});
+wm.onError('importer', onError);
+wm.onResponse('importer', (e) => {
+    let type = e.type;
+    if(type === "showProgress"){
+        showProgressBar(e.title, e.total, () => {});
+    }else if(type === "updateProgress"){
+        emitter.emit('updateProgress', {
+            progress: e.progress,
+        });
+    }else if(type === "done"){
+        emitter.emit('AppOptions#recalcSpace');
+    }
+});
+wm.onError('exporter', onError);
+wm.onResponse('exporter', (e) => {
+    let type = e.type;
+    let ready = e.ready;
+    if(type == "favourites"){
+        if(ready){
+            let gen = e.filedata;
+            const a = document.createElement("a");
+            a.href = 'data:text/json;charset=utf-8,' + JSON.stringify(gen);
+            a.download = "favouritesFlowers.json";
+            a.click();
+        }else{
+            emitter.emit('updateProgress', {
+                progress: e.progress,
+            });
+        }
+    }else if(type == "all"){
+        if(ready){
+            let gen = e.filedata;
+            const a = document.createElement("a");
+            a.href = 'data:text/json;charset=utf-8,' + JSON.stringify(gen);
+            a.download = "localFlowers.json";
+            a.click();
+        }else{
+            emitter.emit('updateProgress', {
+                progress: e.progress,
+            });
+        }
+    }
+});
+
+onBeforeUnmount(() => {
+    wm.deleteWorker('redraw');
+    wm.deleteWorker('exporter');
+    wm.deleteWorker('importer');
+});
+
+const deleteAllFlowers = () => {
+    emitter.emit('showYesNo', {
+        title: 'delete all flowers',
+        message: 'Are you sure you want to delete all flowers?',
+        btnNo: 'no',
+        btnYes: 'Delete all',
+        onConfirm: async (dialog) => {
+            dialog.close();
+            store.localSelected.flowers = [];
+            store.localSelected.index = 0;
+            await store.db.delete();
+            store.db.open();
+            emitter.emit('AppOptions#recalcSpace');
+        },
+    });
+};
+const deleteNonFavourites = () => {
+    emitter.emit('showYesNo', {
+        title: 'Delete non favourites',
+        message: 'Are you sure you want to delete all flowers but favourites?',
+        btnNo: 'no',
+        btnYes: 'Delete non favourites',
+        onConfirm: async (dialog) => {
+            let ids = await store.db.favourites.toArray();
+            let flowers = await store.db.flowers.bulkGet(ids);
+            let descs = await store.db.descriptions.bulkGet(ids);
+            for(let id = 0;id < flowers.length; ++id){
+                ids[id] = id + 1;
+                flowers[id].id = id + 1;
+                if(descs[id] !== undefined){
+                    descs[id].id = id + 1;
+                }
+            }
+            descs = descs.filter((d) => {
+                return d !== undefined;
+            });
+            store.localSelected.flowers = [];
+            store.localSelected.index = 0;
+            store.db.delete();
+            store.db.open();
+            store.db.flowers.bulkAdd(flowers);
+            dialog.close();
+            await store.db.favourites.bulkAdd(ids, ids);
+            await store.db.descriptions.bulkAdd(descs);
+            emitter.emit('AppOptions#recalcSpace');
+        }
+    });
+};
+const redrawLocalFlowers = async () => {
+    wm.sendRequest('redraw', {
+        batchSize: store.settings.limit,
+        params: structuredClone(toRaw(store.settings.params))
+    });
+};
+const showRedrawFlowers = () => {
+    emitter.emit('showYesNo', {
+        title: 'Redraw local flowers',
+        message: 'Are you sure you want to redraw local flowers? (this won\'t override their parameters but you will need to import them to restore to originals)',
+        btnNo: 'no',
+        btnYes: 'redraw local flowers',
+        onConfirm: (dialog) => {
+            store.db.flowers.count((totalFlowers) => {
+                if(totalFlowers != 0){
+                    showProgressBar('re-drawing flowers', totalFlowers, redrawLocalFlowers);   
+                }else{
+                    store.errors.push({message: "You have no local flowers to redraw."});
+                }
+            });
+            dialog.close();
+        },
+    });
+};
+const showExport = (type) => {
+    if(type == "favourites"){
+        emitter.emit('showYesNo', {
+            title: 'Export Favourite flowers',
+            message: 'Are you sure you want to export your favourite flowers?',
+            btnNo: 'no',
+            btnYes: 'Export favourite flowers',
+            onConfirm: (dialog) => {
+                store.db.favourites.count((totalFlowers) => {
+                    if(totalFlowers != 0){
+                        showProgressBar('exporting favourite flowers', totalFlowers, 
+                            () => {
+                                exportFlowers(type);
+                            });
+                    }else{
+                        store.errors.push({message: "You have no favourites to export"});
+                    }
+                });
+                dialog.close();
+            }
+        });
+    }else if(type == "all"){
+        emitter.emit('showYesNo', {
+            title: 'Export all flowers',
+            message: 'Are you sure you want to export all local flowers?',
+            btnNo: 'no',
+            btnYes: 'Export all local flowers',
+            onConfirm: (dialog) => {
+                store.db.flowers.count((totalFlowers) => {
+                    if(totalFlowers != 0){
+                        showProgressBar('exporting flowers', totalFlowers, 
+                            () => {
+                                exportFlowers(type);
+                            });
+                    }else{
+                        store.errors.push({message: "You have no flowers to export"});
+                    }
+                });
+                dialog.close();
+            }
+        });
+    }else if(type == "garden"){
+        emitter.emit('showYesNo', {
+            title: 'Export all garden flowers',
+            message: 'Are you sure you want to export all garden flowers?',
+            btnNo: 'no',
+            btnYes: 'Export all garden flowers',
+            onConfirm: (dialog) => {
+                exportFlowers(type);
+                dialog.close();
+            }
+        });
+    }
+};
+const importFiles = async (files, toFavs) => {
+    if(!files.length){
+        store.errors.push({message: "You must upload at least one json file."});
+        return;
+    }
+    wm.sendRequest('importer', {
+        files: files,
+        toFavs: toFavs,
+        batchSize: store.settings.limit
+    });
+};
+const importGarden = async () => {
+    let files = new Array();
+    files.push(new File([sessionStorage.getItem(STORAGE_KEY_GARDEN)], "gardenFlowers.json", {
+        type: "application/json",
+    }));
+    const toFavs = false;
+    wm.sendRequest('importer', {
+        files: files,
+        toFavs: toFavs,
+        batchSize: store.settings.limit
+    });
+};
+const showImport = (toFavs) => {
+    emitter.emit('showImport', {
+        title: "Import Flowers",
+        btnCancel: "Cancel",
+        btnUpload: "Import Files",
+        onUpload: async (dialog, files) => {
+            await importFiles(files, toFavs);
+            dialog.close();
+        }
+    });
+};
+const showProgressBar = (title, total, fn) => {
+    emitter.emit('showProgress', {
+        title: title,
+        progress: 0,
+        total: total,
+        onLoad: fn
+    });
+};
+const exportFlowers = (type) => {
+    if(type == "garden"){
+        const a = document.createElement("a");
+        a.href = 'data:text/json;charset=utf-8,' + sessionStorage.getItem(STORAGE_KEY_GARDEN);
+        a.download = "gardenFlowers.json";
+        a.click();
+        return;
+    }
+    wm.sendRequest('exporter', {
+        type: type,
+        batchSize: store.settings.limit
+    });
+};
+
+</script>
+
+<style scoped>
+    #actions-settings{
+        padding: 0.6rem;
+        text-align: center;
+        background-color: rgb(37, 39, 41);
+        font-size: 1.25rem;
+        display: flex;
+        flex-flow: row nowrap;
+    }
+    #actions-safe{
+        padding: 0.6rem;
+        margin: 0.6rem;
+        background-color: green;
+        border: solid lightgreen;
+        width: 100%;
+        border-radius: 1.25rem;
+    }
+    .columns {
+        display: flex;
+        gap: 10px;
+        margin-bottom: 15px;
+        justify-content: center;
+    }
+    .column {
+        width: 100%;
+        padding: 0.125rem;
+        overflow-y: auto;
+    }
+    .safe-button{
+        position: relative;
+        font-size: 1.25rem;
+        border-radius: 19.6rem 20.9rem 9.6rem 8.4rem;
+        margin: 0.6rem 0.6rem 0rem 1.25rem;
+        cursor: pointer;
+        border-color: lightgreen;
+        background-color: green;
+        color: lightgreen;
+    }
+    .safe-button:hover{
+        background-color: lightgreen;
+        border-color: green;
+        color: green;
+    }
+    #actions-danger{
+        border: solid red;
+        border-radius: 1.25rem;
+        margin: 0.6rem;
+        padding: 0.6rem;
+        margin-bottom: 1.25rem;
+        width: 100%;
+        text-align: center;
+        background-color: green;
+    }
+    .unsafe-button{
+        position: relative;
+        font-size: 1.25rem;
+        border-radius: 19.6rem 20.9rem 9.6rem 8.4rem;
+        margin: 0.6rem 0.6rem 0rem 1.25rem;
+        cursor: pointer;
+        border-color: whitesmoke;
+        background-color: red;
+        color: whitesmoke;
+    }
+    .unsafe-button:hover{
+        border-color: red;
+        background-color: whitesmoke;
+        color: red;
+    }
+    @media only screen and (max-width: 1280px){
+        #actions-settings{
+            font-size: 0.9rem;
+            flex-flow: row wrap;
+        }
+        .safe-button{
+            font-size: 0.9rem;
+        }
+        .unsafe-button{
+            font-size: 0.9rem;
+        }
+        #actions-safe{
+            margin: 0.2rem;
+        }
+        #actions-danger{
+            margin: 0.2rem;
+        }
+    }
+
+</style>
+
---
+++ src/components/AppOptions.vue
@@ -0,0 +1,236 @@
+<template>
+  <div id="settings-options" class="settings-box">
+    <h2>Options</h2>
+    <div id="pagination-option" class="option-box labelInputArea">
+      <ToolTip :info="'if checked pagination will be used, infinite Scroll otherwise.'" />
+      <label for="pagination">Pagination: </label>
+      <input id="pagination" v-model="store.settings.pagination" type="checkbox" @change="saveSettings()">
+    </div>
+    <div id="loadDemoFlowers-option" class="option-box labelInputArea">
+      <ToolTip :info="'if checked the demo flowers will load in Local.'" />
+      <label for="loadDemoFlowers">Load Demo Flowers: </label>
+      <input id="loadDemoFlowers" v-model="store.settings.loadDemoFlowers" type="checkbox" @change="saveSettings()">
+    </div>
+    <div id="loadModel-option" class="option-box labelInputArea">
+      <ToolTip :info="'if checked the model will download / load when the website is loaded.'" />
+      <label for="loadModel">Load Model: </label>
+      <input id="loadModel" v-model="store.settings.loadModel" type="checkbox" @change="saveSettings(); loadModel();">
+    </div>
+    <div id="persists-option" class="option-box labelInputArea">
+      <ToolTip :info="'it will keep data even when low on space'" />
+      <label for="persist">Persistent storage: </label>
+      <input id="persist" v-model="data.persisted" type="checkbox" @change="persist()">
+    </div>
+    <div>
+      <div id="limit-settings" class="option-box labelInputArea">
+        <ToolTip :info="'limit for how many flowers it will load at a time.'" />
+        <label for="setLimit">Limit per Page: </label>
+        <input id="setLimit" v-model.number="store.settings.limit" type="number" min="1" @change="validateLimit()">
+      </div>
+      <div style="color: lightgreen; text-align: center;"> 
+        <ToolTip :info="'Space used by the flowers (usage / quota) if persistent storage is enabled it will use a bit more space, the model adds around 240mb'" />
+        {{ data.spaceUsage.toFixed(2) }} / {{ data.spaceQuota.toFixed(2) }} MB
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+import { reactive, inject, onBeforeUnmount, onMounted } from 'vue';
+import ToolTip from '../components/ToolTip.vue';
+import { useFlowersStore, STORAGE_KEY } from '../store';
+
+const store = useFlowersStore();
+let emitter = inject('emitter');
+
+const data = reactive({
+    persisted: false,
+    spaceUsage: 0.0,
+    spaceQuota: 0.0
+});
+
+const loadModel = () => {
+    if(store.settings.loadModel){
+        emitter.emit('App#loadModel');
+    }
+};
+
+const persist = async () => {
+    if(!navigator.storage && !navigator.storage.persist){
+        return;
+    }
+    data.persisted  = await navigator.storage.persist();
+};
+const isPersisted = async () => {
+    if(!navigator.storage && !navigator.storage.persisted){
+        return false;
+    }
+    return await navigator.storage.persisted();
+};
+const calcSpace = async () => {
+    if(navigator.storage && navigator.storage.estimate){
+        const estimation = await navigator.storage.estimate();
+        data.spaceUsage = (estimation.usage / 1e+6);
+        data.spaceQuota = (estimation.quota / 1e+6);
+    }else{
+        data.spaceUsage = 0.0;
+        data.spaceQuota = 0.0;
+    }
+};
+onMounted(() => {
+    isPersisted().then(p => data.persisted = p);
+    calcSpace();
+    emitter.on('AppOptions#recalcSpace', () => {
+        calcSpace();
+    });
+});
+onBeforeUnmount(() => {
+    emitter.off('AppOptions#recalcSpace');
+});
+
+const saveSettings = () => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(store.settings));
+};
+const validateLimit = () => {
+    store.settings.limit = validateInteger(store.settings.limit, 100);
+    saveSettings();
+};
+
+const validateInteger = (number, Default) => {
+    if(Number.isNaN(parseInt(number))){
+        return Default;
+    }
+    return number;
+};
+
+</script>
+
+<style scoped>
+    #settings-options{
+        margin: 1em 1em;
+        background-color: green;
+    }
+    #params-settings{
+        display: flex;
+        flex-flow: column wrap;
+        margin: 1em 1em;
+        background-color: green;
+    }
+    .labelInputArea {
+        display: block;
+        padding-top: 0.31rem;
+    }
+    .labelInputArea label{
+        width: 18.3rem;
+        display: inline-block;
+        margin: 0rem 0rem 0rem 0.93rem;
+    }
+    .labelInputArea input[type=checkbox]{
+        background-color: green;
+        color:lightgreen;
+        border: solid lightgreen;
+        cursor: pointer;
+        display: inline-grid;
+        place-content: center;
+        grid-template-columns: 0.5em auto;
+        gap: 0.1em;
+        font-size: 2rem;
+        font-weight: bold;
+        -webkit-appearance: none;
+        appearance: none;
+        box-shadow: inset 0rem 0rem 0.2rem 0.125rem black;
+    }
+    .labelInputArea input[type="checkbox"]::before {
+        content: "";
+        width: 0.65em;
+        height: 0.65em;
+        transform: scale(0);
+        transition: 120ms transform cubic-bezier(0.175, 0.885, 0.32, 1.275);
+        box-shadow: inset 2em 2em lightgreen;
+        transform-origin: top left;
+        clip-path: polygon(14% 44%, 0 65%, 50% 100%, 100% 16%, 80% 0%, 43% 62%);
+    }
+    .labelInputArea input[type="checkbox"]:checked::before {
+        transform: scale(1);
+    }
+    .labelInputArea input[type=number]{
+        vertical-align:top;
+        background-color: green;
+        color: lightgreen;
+        border: solid lightgreen;
+        border-radius: 8rem;
+        margin: 0.125rem 0rem 0rem 0.125rem;
+        width: 15%;
+        text-align: center;
+    }
+    .safe-button{
+        position: relative;
+        font-size: 1.25rem;
+        border-radius: 19.6rem 20.9rem 9.6rem 8.4rem;
+        margin: 0.6rem 0.6rem 0rem 1.25rem;
+        cursor: pointer;
+        border-color: lightgreen;
+        background-color: green;
+        color: lightgreen;
+    }
+    .safe-button:hover{
+        background-color: lightgreen;
+        border-color: green;
+        color: green;
+    }
+    .unsafe-button{
+        position: relative;
+        font-size: 1.25rem;
+        border-radius: 19.6rem 20.9rem 9.6rem 8.4rem;
+        margin: 0.6rem 0.6rem 0rem 1.25rem;
+        cursor: pointer;
+        border-color: whitesmoke;
+        background-color: red;
+        color: whitesmoke;
+    }
+    .unsafe-button:hover{
+        border-color: red;
+        background-color: whitesmoke;
+        color: red;
+    }
+    .settings-box {
+        border: 0.25rem solid lightgreen;
+        padding: 0.6rem;
+        margin-bottom: 1.25rem;
+        border-radius: 2rem;
+    }
+    .settings-box h2{
+        text-align: center;
+    }
+    .option-box {
+        margin-bottom: 0.6rem;
+    }
+    @media only screen and (max-width: 1280px){
+        .safe-button{
+            font-size: 0.9rem;
+        }
+        .unsafe-button{
+            font-size: 0.9rem;
+        }
+        .labelInputArea label{
+            width: 10.6rem;
+            display: inline-block;
+            margin: 0rem 0rem 0rem 0.93rem;
+        }
+        #settings-container{
+            font-size: 0.9rem;
+            grid-template-columns: repeat(auto-fill, minmax(100%, 1fr));
+            width: 100%;
+        }
+        .labelInputArea input[type=number]{
+            width: 20%;
+        }
+    }
+    input[type=number] {
+        appearance: textfield;
+        box-shadow: inset 0rem 0rem 0.3rem 0.125rem black;
+    }
+
+</style>
+
